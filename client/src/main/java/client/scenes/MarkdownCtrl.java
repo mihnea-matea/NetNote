7d731214794f88a9c1a9d49def7b16fd29224f52
Bianca Radoi <b.radoi@student.tudelft.nl>
2024-11-27 23:08:43 +0100

    Markdown format

--- client/src/main/java/client/scenes/MarkdownCtrl.java
+++ client/src/main/java/client/scenes/MarkdownCtrl.java
@@ -1,22 +1,24 @@
 package client.scenes;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.web.WebView;
 import org.commonmark.node.*;
 import org.commonmark.parser.Parser;
 import org.commonmark.renderer.html.HtmlRenderer;
-
 public class MarkdownCtrl {
     @FXML
     private TextArea markdownTitle;
 
     @FXML
-    private Label htmlText;
+    private WebView htmlText;
 
     @FXML
-    private Label htmlTitle;
+    private WebView htmlTitle;
 
     @FXML
     private TextArea markdownText;
@@ -28,12 +30,20 @@ public class MarkdownCtrl {
 
     @FXML
     public void initialize(){
+        markdownText.setOnKeyPressed(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent keyEvent) {
+                if(keyEvent.getCode()== KeyCode.ENTER){
+                    enterPress();
+                }
+            }
+        });
         generateMarkdownTitle();
         generateMarkdownText();
-        renderMarkdownToHTML(markdownText, htmlText);
         renderMarkdownToHTML(markdownTitle, htmlTitle);
+        renderMarkdownToHTML(markdownText, htmlText);
     }
-    private void renderMarkdownToHTML(TextArea markdown, Label html) {
+    private void renderMarkdownToHTML(TextArea markdown, WebView html) {
         if(markdown !=null){
             markdown.textProperty().addListener(new ChangeListener<String>() {
                 @Override
@@ -43,7 +53,7 @@ public class MarkdownCtrl {
                     }
                     Node doc= parserM.parse(t1);
                     String htmlString= rendererH.render(doc);
-                    html.setText(htmlString);
+                    html.getEngine().loadContent(htmlString);
                 }
             });
         }
@@ -53,7 +63,8 @@ public class MarkdownCtrl {
         Node document = new Document();
         Heading heading = new Heading();
         heading.setLevel(2);
-        heading.appendChild(new Text("#Add a title"));
+        Text content=new Text("# Add a title");
+        heading.appendChild(content);
         document.appendChild(heading);
         if(markdownTitle!=null){
             Text text=(Text)(document.getFirstChild().getFirstChild());
@@ -77,5 +88,10 @@ public class MarkdownCtrl {
             Text text = (Text)(document.getFirstChild().getFirstChild());
             markdownText.setText(text.getLiteral());
         }
+    }
+    public void enterPress(){
+        String text= markdownText.getText();
+        markdownText.setText(text+"\n");
+        markdownText.positionCaret(markdownText.getLength());
     }
 }
